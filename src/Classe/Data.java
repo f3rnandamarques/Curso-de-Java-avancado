a3d7f9d2628c191c1ccc3541182e485d938d4db0
Fernanda Marques <f3rnandamarques@gmail.com>
2023-11-06 12:28:42 -0300

    Fundamentos de Java

--- src/Classe/Data.java
+++ src/Classe/Data.java
@@ -6,18 +6,24 @@ public class Data {
     int ano;
 
     Data() {
-        dia = 1;
-        mes = 1;
-        ano = 1970;
+
+        // dia = 1;
+        // mes = 1;
+        // ano = 1970;
+        this(1, 1, 1970);
     }
     Data(int diaInicial, int mesInicial, int anoInicial) {
-        dia = diaInicial;
-        mes = mesInicial;
-        ano = anoInicial;
+        this.dia = diaInicial;
+        this.mes = mesInicial;
+        this.ano = anoInicial;
     }
 
     String obterDataFormatada() {
-        return String.format("%d/%d/%d", dia, mes, ano);
+        final String formato = "%d/%d/%d";
+        return String.format(formato, dia, mes, ano);
+    }
+    void imprimirDataFormatada() {
+        System.out.println(obterDataFormatada());
     }
 
 }
