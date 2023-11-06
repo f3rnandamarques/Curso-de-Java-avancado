ee55bbd834fbf08a263a1a3bf91210b81cb3e847
Fernanda Marques <f3rnandamarques@gmail.com>
2023-11-06 16:45:00 -0300

    Fundamentos de Java

---
+++ src/Classe/Desafio/Jantar.java
@@ -0,0 +1,21 @@
+package Classe.Desafio;
+
+public class Jantar {
+    public static void main(String[] args) {
+
+        Comida c1 = new Comida("Arroz", 0.8);
+        Comida c2 = new Comida("Feijão", 0.5);
+
+        Pessoa p = new Pessoa("Fernanda", 60);
+
+        System.out.println(p.apresentar());
+
+        p.comer(c1);
+        System.out.println(p.apresentar());
+
+        p.comer(c1);
+        p.comer(c2);
+        System.out.println(p.apresentar());
+
+    }
+}
