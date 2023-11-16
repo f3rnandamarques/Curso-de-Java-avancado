eaf271bffd3e73b8f6143ed8f6610a345b32842b
Fernanda Marques <f3rnandamarques@gmail.com>
2023-11-16 00:15:33 -0300

    Fundamentos de Java

---
+++ src/oo/composicao/heranca/desafio/Item.java
@@ -0,0 +1,12 @@
+package oo.composicao.heranca.desafio;
+
+public class Item {
+
+    Produto produto;
+    int quantidade;
+
+    Item(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+}
